3a72a2202b286f26e599d3b57ddc514d65d29b7c
guccibase <tahiru@tryhungry.com>
2021-04-10 21:42:02 -0400

    web

---
+++ server.js
@@ -0,0 +1,30 @@
+const { static } = require('express');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const {v4:uuidV4} = require('uuid');
+
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+
+app.get('/', (req, res)=>{
+    res.redirect(`/${uuidV4()}`);
+})
+
+app.get('/:rooms', (req, res)=>{
+    res.render('room', {roomId:req.params.room})
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(3000);
